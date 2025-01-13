b31675be29e2bf9299b9739f847f729facceaa6e
waschlow <rico.waschlowsky@student.reutlingen-university.de>
2025-01-13 18:34:42 +0100

    Minor changes
    Modified:
      Reference:
        - added Reference.to(Id<T> id)-method

--- src/de/ehealth/evek/api/type/Reference.java
+++ src/de/ehealth/evek/api/type/Reference.java
@@ -14,8 +14,6 @@ public record Reference<T>(Id<T> id) implements Serializable{
 	private static final long serialVersionUID = -1648976582316498583L;
 
 	/**
-	 * method to
-	 * <p>
 	 * Method to create a reference object to a specified id
 	 * @param <T> - the type of the referenced object
 	 * @param id - the id of the referenced object
@@ -24,6 +22,17 @@ public record Reference<T>(Id<T> id) implements Serializable{
 	 */
 	public static <T> Reference<T> to(String id){
 		return new Reference<>(new Id<>(id));
+	}
+	
+	/**
+	 * Method to create a reference object to a specified id
+	 * @param <T> - the type of the referenced object
+	 * @param id - the id of the referenced object
+	 * 
+	 * @return Reference<T> - the new reference to the object
+	 */
+	public static <T> Reference<T> to(Id<T> id){
+		return new Reference<>(id);
 	}
 	
 	@Override
